9d2b3122d62f90b10bbd55700bcb297bb915d148
Dgley Ramos <dgleysilva@gmail.com>
2022-12-07 00:00:39 -0300

    Adicionando usuario no banco de dados

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,4 +1,4 @@
-
+import prismaClient from "../../prisma"
 interface UserRequest{
     name: String
     email: String
@@ -8,6 +8,37 @@ interface UserRequest{
 class CreateUserService {
     async execute({name, email, password}: UserRequest){
         
+        //verificar se enviou o email
+        if(!email){
+            throw new Error("Email incorrect")
+        }
+
+        //Verificar se esse email já esta cadastrado na plataforma
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where:{
+                email: `${email}`
+            }
+        })
+
+        if(userAlreadyExists){
+            throw new Error("User already exists");
+        }
+
+        const user = await prismaClient.user.create({
+            data: {
+                name: `${name}`,
+                email: `${email}`,
+                password: `${password}`
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true
+            }
+        })
+
+
+        return user;
     }
 }
 
