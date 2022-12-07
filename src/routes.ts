f56089d24fee55441e220e4f156242c6574de21a
Dgley Ramos <dgleysilva@gmail.com>
2022-12-06 23:34:08 -0300

    Criando service e controller para criar usuario

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,12 @@
 
-import {Router, Request, Response} from 'express'
+import {Router} from 'express'
+import { CreateUserController } from './controllers/user/CreateUserController';
 
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
-    return res.json({ok: true})
-})
 
+//-- ROTAS USER --
+router.post('/users', new CreateUserController().handle)
 
 export {router};
